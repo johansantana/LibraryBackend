6a7b4d74fd72c6e5a8d644c7284cea36f12ea23f
Johan Santana <johancarlosst@gmail.com>
2025-09-03 18:46:23 -0400

    feat: initial

---
+++ Domain/Author.cs
@@ -0,0 +1,9 @@
+namespace Domain;
+
+class Author
+{
+    public int Id { get; set; }
+    public int IdBook { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
---
+++ Domain/Book.cs
@@ -0,0 +1,11 @@
+namespace Domain;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public int PageCount { get; set; }
+    public string? Excerpt { get; set; }
+    public DateTime PublishDate { get; set; }
+}
